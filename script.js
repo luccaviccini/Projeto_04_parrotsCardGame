9277cca0333126f112bbb4f242480780a20c87fa
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2022-10-21 14:38:24 -0300

    adding cards based on number typed in prompt

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 let nCards = Number(prompt("How many cards do you want to play with? Type an even number between 4 and 14"));
 // check if the number of cards is even
 checkConditions();
+//add cards
+addCards(nCards);
+console.log(nCards);
+
+
+
+
+
+
+
+
 function checkConditions(){
     while((nCards < 4) || (nCards > 14) || (nCards%2 != 0)){            
         nCards =  Number(prompt("Type an even number between 4 and 14"));
     }
 }
 
+function addCards(nCards){
+    let deck = document.querySelector(".deck");
+    for(let i = 0; i < nCards; i++){
+        const card = document.createElement("div");
+        card.classList.add("card");
+        deck.appendChild(card);
+    }
+}
